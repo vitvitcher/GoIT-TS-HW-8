6a9259a8def89f8854e4e329c8629aa4de021baf
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2026-04-18 23:01:09 +0200

    homework 8 not-found file revert

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,11 @@
 'use client';
-import { Metadata } from 'next';
+// import type { Metadata } from "next";
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import css from './Home.module.css'
 
 
-export const metadata: Metadata = {
-    title: "NoteHub page not found",
-    description: "Unfortunately the page that you are looking for does not exist",
-    openGraph: {
-        title: `NoteHub page not found`,
-        description: "Unfortunately the page that you are looking for does not exist.",
-        url: `https://notehub.com/`,
-        images: [
-            {
-                url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-                width: 1200,
-                height: 630,
-                alt: 'NoteHub Logo',
-            },
-        ],
-    },
-}
+
 
 const NotFound = () => {
     const router = useRouter();
